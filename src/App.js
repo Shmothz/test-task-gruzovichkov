1180bc65a9e85825ec10172bf3500c77829f7561
Shmothz <retdetwet@gmail.com>
2021-07-11 17:34:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import Grid from "@material-ui/core"
+import {createUseStyles} from 'react-jss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Page(props) {
+  return <MyWonderfulComponent {...props}>
+    I'm text from a component
+  </MyWonderfulComponent>
 }
 
-export default App;
+function MyWonderfulComponent({id, options, children, other}) {
+
+  const styles = createUseStyles({
+    title: {
+      color: 'red'
+    }
+  })()
+
+  const [summ, setSumm] = useState(other);
+
+  useEffect(() => {
+    if (id && options.params.fields.isDynamic) {
+      setSumm(summ + 1);
+    }
+  });
+
+  return (
+    <>
+      <h1 className={styles.title}>Hello World!</h1>
+      <Grid>
+        <Grid xs={12}>{children}</Grid>
+        <Grid>{summ}</Grid>
+      </Grid>
+    </>
+  )
+}
